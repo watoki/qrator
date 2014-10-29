d9818557b50a3cebb7e53418d3629ae295c694cb
Nikolas Martesn <nikolas.m@rtens.org>
2014-10-29 22:18:51 +0100

    Added CallbackProperty

---
+++ src/watoki/cqurator/representer/CallbackProperty.php
@@ -0,0 +1,33 @@
+<?php
+namespace watoki\cqurator\representer;
+
+class CallbackProperty extends Property {
+
+    /** @var null|callable */
+    private $getCallback;
+
+    /** @var null */
+    private $setCallback;
+
+    public function __construct($object, $name, $getCallback = null, $setCallback = null) {
+        parent::__construct($object, $name);
+        $this->getCallback = $getCallback;
+        $this->setCallback = $setCallback;
+    }
+
+    public function canGet() {
+        return !!$this->getCallback;
+    }
+
+    public function canSet() {
+        return !!$this->setCallback;
+    }
+
+    public function get() {
+        return call_user_func($this->getCallback);
+    }
+
+    public function set($value) {
+        call_user_func($this->setCallback, $value);
+    }
+}
